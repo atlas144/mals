05b83c78f0d1b3d8af87db0db5f1fdaa11ac881f
Jakub Švarc <jakubsvarc@protonmail.com>
2021-12-22 23:19:14 +0100

    feature: component structure initialized

---
+++ control/main-control-unit/src/maincontrolunit/MainControlUnit.java
@@ -0,0 +1,13 @@
+package maincontrolunit;
+
+/**
+ *
+ * @author atlas144
+ */
+public class MainControlUnit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
