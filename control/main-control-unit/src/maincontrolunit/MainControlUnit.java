082b3bc140fb8fdf751b01f9078631cbd094783a
Jakub Švarc <jakubsvarc@protonmail.com>
2022-01-03 22:28:38 +0100

    feat: added MalsControlLibrary

--- control/main-control-unit/src/maincontrolunit/MainControlUnit.java
+++ control/main-control-unit/src/maincontrolunit/MainControlUnit.java
@@ -1,13 +1,28 @@
 package maincontrolunit;
 
+import malscontrolplatform.MalsControlPlatform;
+
 /**
  *
  * @author atlas144
  */
 public class MainControlUnit {
+    
+    private final String webHost = "localhost";
+    private final short webPort = 8080;
+    
+    private final MalsControlPlatform controlPlatform;
+    
+    public MainControlUnit() {
+        controlPlatform = new MalsControlPlatform(webHost, webPort);
+        
+        // register modules
+        
+        controlPlatform.start();
+    }
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        new MainControlUnit();
     }
     
 }
